f7004f1ed3268386dd7e700c2b60fc032609ccb4
Martinius Feragen <andrmf18@uia.no>
2021-03-10 16:56:33 +0100

    added recycler view

--- app/src/main/java/no/uia/todo/MainActivity.kt
+++ app/src/main/java/no/uia/todo/MainActivity.kt
@@ -2,10 +2,28 @@ package no.uia.todo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.view.*
+import no.uia.todo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val LOG_TAG = "TODO:MainActivity"
+    private lateinit var binding: ActivityMainBinding
+
+    // Behold! My stuff!
+    val listOfMyStuff = mutableListOf("vespa", "battle axe", "m16", "statue", "shake Weight", "blarg", "chest", "loot", "more loot")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        Log.d(LOG_TAG, "blarg")
+        view.todoRecycler.layoutManager = LinearLayoutManager(this)
+        Log.d(LOG_TAG, "Foo")
+        view.todoRecycler.adapter = ToDoAdapter(listOfMyStuff)
     }
 }
---
+++ app/src/main/java/no/uia/todo/ToDoAdapter.kt
@@ -0,0 +1,25 @@
+package no.uia.todo
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import no.uia.todo.databinding.TodoCardBinding
+
+class ToDoAdapter(private val todoItems: MutableList<String>): RecyclerView.Adapter<ToDoAdapter.ViewHolder>() {
+
+    class ViewHolder(private val binding: TodoCardBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(todoItem: String){
+            binding.placeholder.text = todoItem
+        }
+    }
+
+    override fun getItemCount(): Int = todoItems.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(todoItems[position])
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(TodoCardBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+}
